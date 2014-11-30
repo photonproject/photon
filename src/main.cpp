3e83337d61383ef9be564f3a4d0c187f16e0847c
BlueDragon747 <bluedragon747@gmail.com>
2014-11-30 09:56:44 +0000

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -78,7 +78,7 @@ int64 nHPSTimerStart = 0;
 
 // Settings
 int64 nTransactionFee = 0;
-
+int64 nMinimumInputValue = DUST_HARD_LIMIT;
 
 
 //////////////////////////////////////////////////////////////////////////////
