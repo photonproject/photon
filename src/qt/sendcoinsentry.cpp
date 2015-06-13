1255ba20d9899a77ab0f5f20d19f4f6a449502cd
cinnamon_carter <cinnamoncoin@users.noreply.github.com>
2015-06-13 03:40:23 -0400

    add valid photon address to gui
    add valid photon address to gui

--- src/qt/sendcoinsentry.cpp
+++ src/qt/sendcoinsentry.cpp
@@ -24,7 +24,7 @@ SendCoinsEntry::SendCoinsEntry(QWidget *parent) :
 #if QT_VERSION >= 0x040700
     /* Do not move this to the XML file, Qt before 4.7 will choke on it */
     ui->addAsLabel->setPlaceholderText(tr("Enter a label for this address to add it to your address book"));
-    ui->payTo->setPlaceholderText(tr("Enter a Photon address (e.g. 1NS17iag9jJgTHD1VXjvLCEnZuQ3rJDE9L)"));
+    ui->payTo->setPlaceholderText(tr("Enter a Photon address (e.g. BXgB67NbzKf4CP3TXhQumTsyEJa3eGrZ56)"));
 #endif
     setFocusPolicy(Qt::TabFocus);
     setFocusProxy(ui->payTo);
