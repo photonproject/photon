06525c5f640136165102e1e7bf56e9eab4978665
BlueDragon747 <bluedragon747@gmail.com>
2016-10-24 15:05:34 +0100

    set to match OP example conf
    range issue really needs to be <65000 set to match example conf "rpcport=8984"

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -40,7 +40,7 @@ static boost::thread_group* rpc_worker_group = NULL;
 
 static inline unsigned short GetDefaultRPCPort()
 {
-    return GetBoolArg("-testnet", false) ?  18998: 74532;
+    return GetBoolArg("-testnet", false) ?  18998: 8984;
 }
 
 Object JSONRPCError(int code, const string& message)
